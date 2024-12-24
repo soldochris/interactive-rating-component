afd7a2e3fb4fbf04cbe1d2813a09b6eea63a479c
Christian Soldevilla <soldevilla.christian@gmail.com>
2024-12-23 19:20:12 -0500

    basic styles for Survey component

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -1,33 +1,30 @@
+import './Survey.css'
+
 function Survey(){
   return (
-    <section>
+    <section className="survey">
+      <img src="/images/icon-star.svg" alt="" />
       <h1>How did we do?</h1>
       <p>
         Please let us know how we did with your support request. All feedback is appreciated 
         to help us improve our offering!
       </p>
-      <form action="">
+      <form>
         <div>
-          <label htmlFor="">
-            1
-            <input type="radio" name="1" id="1" />
-          </label>
-          <label htmlFor="">
-            2
-            <input type="radio" name="2" id="2" />
-          </label>
-          <label htmlFor="">
-            3
-            <input type="radio" name="3" id="3" />
-          </label>
-          <label htmlFor="">
-            4
-            <input type="radio" name="4" id="4" />
-          </label>
-          <label htmlFor="">
-            5
-            <input type="radio" name="5" id="5" />
-          </label>
+          <label htmlFor="1">1</label>
+          <input type="radio" name="value" id="1" value="1"/>
+
+          <label htmlFor="2">2</label>
+          <input type="radio" name="value" id="2" value="2"/>
+
+          <label htmlFor="3">3</label>
+          <input type="radio" name="value" id="3" value="3"/>
+
+          <label htmlFor="4">4</label>
+          <input type="radio" name="value" id="4" value="4"/>
+
+          <label htmlFor="5">5</label>
+          <input type="radio" name="value" id="5" value="5"/>
         </div>
         <button>Submit</button>
       </form>
